c83a1cf7501a61d5c9e40a4dbff7cb922326928d
Elias Canales <eliasibz@gmail.com>
2013-04-15 16:12:09 +0200

    Imagen de prueba

--- proyecto/src/vista/Proyecto.java
+++ proyecto/src/vista/Proyecto.java
@@ -9,33 +9,25 @@ import com.badlogic.gdx.graphics.Texture.TextureFilter;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.maps.tiled.renderers.BatchTiledMapRenderer;
 
 public class Proyecto implements ApplicationListener {
 	private OrthographicCamera camera;
 	private SpriteBatch batch;
 	private Texture texture;
-	private Sprite sprite;
+	TextureRegion textura;
 	
 	@Override
 	public void create() {		
 		
-		float w = Gdx.graphics.getWidth();
+	/*	float w = Gdx.graphics.getWidth();
 		float h = Gdx.graphics.getHeight();
+		*/
 		
-		camera = new OrthographicCamera(1, h/w);
 		batch = new SpriteBatch();
-		
-		texture = new Texture(Gdx.files.internal("data/libgdx.png"));
-		texture.setFilter(TextureFilter.Linear, TextureFilter.Linear);
-		
-		TextureRegion region = new TextureRegion(texture, 0, 0, 512, 275);
-		
-		sprite = new Sprite(region);
-		sprite.setSize(0.9f, 0.9f * sprite.getHeight() / sprite.getWidth());
-		sprite.setOrigin(sprite.getWidth()/2, sprite.getHeight()/2);
-		sprite.setPosition(-sprite.getWidth()/2, -sprite.getHeight()/2);
+		texture = new Texture(Gdx.files.internal("data/enemy.png"));
+		textura = new TextureRegion(texture);
 	}
-
 	@Override
 	public void dispose() {
 		batch.dispose();
@@ -47,9 +39,8 @@ public class Proyecto implements ApplicationListener {
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL10.GL_COLOR_BUFFER_BIT);
 		
-		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
-		sprite.draw(batch);
+		batch.draw(textura, 0, 0, 50, 50);
 		batch.end();
 	}
 
